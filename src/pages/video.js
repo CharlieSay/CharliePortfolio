21967fd40e4dd83fdb2b2fca2942706831257ba4
CharlieSay <charlie.w.say@live.co.uk>
2020-05-05 21:24:38 +0100

    Casing that fixes build issue.

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ElementCard from "../components/elementCard"
+import ElementCard from "../components/elementcard"
 
 import content from "../data/videopage.json"
 
